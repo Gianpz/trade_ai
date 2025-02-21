7cc2ab54756a508d8621e086078ae82abe0dc8b7
Gianpz <gianpz@gmail.com>
2025-02-21 18:39:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,14 @@
+from flask import Flask
 import subprocess
 
+app = Flask(__name__)
+
+# Avvia lo script all'avvio del server
 subprocess.run(["python", "pred.py", "model_williams_20.40.h5"])
+
+@app.route('/')
+def home():
+    return "🚀 Server Flask in esecuzione su Render!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)  # Render usa la porta 10000
