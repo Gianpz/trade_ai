b0bb4fce372c585395a39175916546a52a302b19
Gianpz <gianpz@gmail.com>
2025-02-22 19:37:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,21 +11,16 @@ import subprocess
 
 app = Flask(__name__)
 
-def fetch_data():
-    tv = TvDatafeed()
-    df = tv.get_hist(symbol="XAUUSD", exchange="OANDA", interval=Interval.in_15_minute, n_bars=2000)
-    df = df.reset_index().rename(columns={'open': 'Open', 'high': 'High', 'low': 'Low', 'close': 'Close', 'volume': 'Volume', 'datetime': 'Date'})
-    print(df.tail())
-    
-def update_predictions():
-    while True:
-        df_raw = fetch_data()
-        time.sleep(900)  # 15 minuti
-update_predictions()
+tv = TvDatafeed()
+
+@app.route("/dati")
+def get_dati():
+    dati = tv.get_hist(symbol="XAUUSD", exchange="OANDA", interval=Interval.in_15_minute, n_bars=2000)
+    return jsonify(dati.to_dict())
 
 @app.route('/')
 def home():
-    return f"{predicted_candles}"
+    return "vai su /dati"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)  # Render usa la porta 10000
